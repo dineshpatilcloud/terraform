8e1fc2726fcab7328502c6b53fa2e9950e699690
dineshpatilcloud <dineshazpatil@outlook.com>
2022-01-15 13:27:49 +0530

    added it and finance

---
+++ finance.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "finance-rg" {
+  name     = "finance-rg"
+  location = "West Europe"
+}
---
+++ it.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "it-rg" {
+  name     = "it-rg"
+  location = "West Europe"
+}
